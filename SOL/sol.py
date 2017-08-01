dde626e550a78d496eb82075dc52c464654cc49b
João Machado <joao.machado.s.jr@gmail.com>
2017-08-01 07:49:03 -0300

    Comecei a escrever a primeira classe do SOL

---
+++ SOL/sol.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Esse módulo define a classe principal dessa ferramenta
+
+Esse módulo é o módulo de mais alto nível da ferramenta.
+Ele funciona como o ponto de partida para o acesso aos demais módulos.
+"""
+
+class SOL(object):
+
